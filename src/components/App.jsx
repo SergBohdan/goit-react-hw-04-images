53fdb85dfdf553c0432135dff7d719dec4d26850
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-09-18 23:09:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,65 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
-import { fetchImages } from 'api';
 import ImageGallery from './ImageGallery';
+import Button from './Button';
+import Modal from './Modal';
 import Loader from './Loader';
+import { fetchImages } from 'api';
+
 
 function App() {
-  const [query, setQuery] = useState(null);
+  const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState('');
+  const [showButton, setShowButton] = useState(false);
 
-  const handleSearch = async (value) => {
-    setQuery(value);
-    setImages([]);
-    
-    if (value.trim() === '') {
+  useEffect(() => {
+    if (query === '') {
       return;
     }
 
-    setIsLoading(true);
+    fetchImages(query, page)
+      .then(newImages => {
+        setImages(prevImages => [...prevImages, ...newImages]);
+        setShowButton(newImages.length >= 12);
+      })
+      .catch(error => {
+        console.error('Error getting images:', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [query, page]);
 
-    try {
-      const newImages = await fetchImages(query, 1);
-      setImages(newImages);
-    } catch (error) {
-      console.error('Error getting images:', error);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = value => {
+    setQuery(value);
+    setImages([]);
+    setPage(1);
   };
 
-  const handleImageClick = (largeImageURL) => {
-    setShowModal(true);
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleImageClick = largeImageURL => {
     setSelectedImage(largeImageURL);
+    setShowModal(true);
   };
 
   const handleCloseModal = () => {
-    setShowModal(false);
     setSelectedImage('');
+    setShowModal(false);
   };
 
   return (
     <div className="App">
       <Searchbar onSubmit={handleSearch} />
-      {query && (
-        <ImageGallery images={images} onClick={handleImageClick} />
-      )}
+      <ImageGallery images={images} onClick={handleImageClick} />
       {isLoading && <Loader />}
+      {showButton && !isLoading && <Button onLoadMore={handleLoadMore} />}
       {showModal && (
         <Modal largeImageURL={selectedImage} onClose={handleCloseModal} />
       )}
