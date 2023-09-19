45fba3f9bcfd572f32bb06a032482f5bae619605
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-09-19 23:04:56 +0300

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = (event) => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleOverlayClick = (event) => {
+const Modal = ({ largeImageURL, onClose }) => {
+  const handleOverlayClick = (event) => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL } = this.props;
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-    return (
-      <div className="Overlay" onClick={this.handleOverlayClick}>
-        <div className="Modal">
-          <img
-            src={largeImageURL}
-            alt="Large"
-            />
-        </div>
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="Overlay" onClick={handleOverlayClick}>
+      <div className="Modal">
+        <img src={largeImageURL} alt="Large" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Modal;
 
